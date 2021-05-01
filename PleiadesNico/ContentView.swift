5145646c1665333a15ddd2a175005444fcc6b066
YankeeDeltaBravo225 <takahisa2kato@gmail.com>
2021-05-01 19:31:15 +0900

    Initial Commit

---
+++ PleiadesNico/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PleiadesNico
+//
+//  Created by katot on 2021/05/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PleiadesNico/PleiadesNicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PleiadesNicoApp.swift
+//  PleiadesNico
+//
+//  Created by katot on 2021/05/01.
+//
+
+import SwiftUI
+
+@main
+struct PleiadesNicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
